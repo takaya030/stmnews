cb2043a06df273517a9b446c8a3b2148c0e09c85
Minoru Takayama <takaya030@yahoo.co.jp>
2021-04-18 16:44:19 +0900

    add Twitter timeline test controller

---
+++ app/Http/Controllers/TwitterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \Illuminate\Http\Request;
+
+use App\Models\Twitter\Timeline;
+use App\Models\Twitter\Tweet;
+
+class TwitterController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    // test gettine timeline
+	public function getTimeline(Request $request)
+	{
+		$model = new Timeline();
+		$result = $model->getTimeline();
+		dd($result);
+
+		$tweets = [];
+		if( is_array($result) )
+		{
+			foreach( $result as $tw )
+			{
+				$tweets[] = new Tweet( $tw );
+			}
+		}
+
+		return view("timeline", ['tweets' => $tweets]);
+	}
+}
