1f52005e83446e329e2558accd301423d403af0d
Minoru Takayama <takaya030@yahoo.co.jp>
2021-04-24 23:24:00 +0900

    add Twitter user_id config

--- config/accounts.php
+++ config/accounts.php
@@ -16,6 +16,8 @@ return [
 		'client_secret'	=> env('TWITTER_CLIENT_SECRET'),
 		'access_token'	=> env('TWITTER_ACCESS_TOKEN'),
 		'access_token_secret'	=> env('TWITTER_ACCESS_TOKEN_SECRET'),
+
+		'user_id'		=> env('TWITTER_USER_ID'),
 	],
 
 	/**
