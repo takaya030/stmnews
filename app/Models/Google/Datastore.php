659d091fd61529be602bc3122458f0be67a0bcdd
Minoru Takayama <takaya030@yahoo.co.jp>
2021-04-25 00:14:36 +0900

    add Datastore insert methoed

--- app/Models/Google/Datastore.php
+++ app/Models/Google/Datastore.php
@@ -79,6 +79,49 @@ EOM;
 		return $entity;
 	}
 
+	public function insert( string $kind, array $properties )
+	{
+		$googleService = $this->getOauthService();
+		$projectId = config('accounts.google.project_id');
+		$params_str= "";
+
+		foreach( $properties as $key => $value )
+		{
+			if( empty($params_str) )
+				$params_str = static::getPropertyString( $key, $value );
+			else
+				$params_str .= ',' . static::getPropertyString( $key, $value );
+		}
+
+		$body = <<< EOM
+{
+	"mode":"NON_TRANSACTIONAL",
+	"mutations":[
+		{
+			"insert": {
+				"key":{ "partitionId":{ "projectId":"{$projectId}" }, "path":[ {"kind":"{$kind}"} ] },
+				"properties": {
+					{$params_str}
+				}
+			}
+		}
+	 ]
+}
+EOM;
+		// Send a request with it
+		$result = json_decode(
+			$googleService->request(
+				$this->base_url . ':commit',
+				'POST',
+				$body,
+				[ 'Content-type' => 'application/json' ]
+			),
+			true
+		);
+
+		return $result;
+	}
+
 	public function upsert( string $kind, string $name, array $properties )
 	{
 		$googleService = $this->getOauthService();
