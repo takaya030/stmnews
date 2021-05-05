ed5304893b80528443a099c7104b0bf7c463f33f
Minoru Takayama <takaya030@yahoo.co.jp>
2021-05-05 17:23:37 +0900

    add UmaTweet

---
+++ app/Models/Twitter/UmaTweet.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\Twitter;
+
+use App\Models\Google\News\Item as NewsItem;
+
+class UmaTweet extends UmaOAuthClient
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function postNewsItem( NewsItem $news )
+	{
+		return $this->postText( $news->getTitle() . ' ' . $news->getUrl() );
+	}
+
+	public function postText( string $text )
+	{
+		$params = 'status=' . urlencode($text);
+
+		// Send a request with it
+		$result = json_decode( $this->service->request('https://api.twitter.com/1.1/statuses/update.json?'.$params, 'POST'), true);
+
+		return $result;
+	}
+}
