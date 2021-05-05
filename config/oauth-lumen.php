95608186158ecf83a03825a0826a6f80646c2f1d
Minoru Takayama <takaya030@yahoo.co.jp>
2021-05-05 19:25:15 +0900

    add UmaTwitter oauth config

--- config/oauth-lumen.php
+++ config/oauth-lumen.php
@@ -26,6 +26,11 @@ return [
 			'client_secret' => env('TWITTER_CLIENT_SECRET'),
 			//'scope'         => [],				// No scope - oauth1 doesn't need scope
 		],
+		'UmaTwitter' => [
+			'client_id'     => env('UMAMUSU_CLIENT_ID'),
+			'client_secret' => env('UMAMUSU_CLIENT_SECRET'),
+			//'scope'         => [],				// No scope - oauth1 doesn't need scope
+		],
 		'MyGoogle' => [
 			'client_id'     => env('GOOGLE_CLIENT_ID'),
 			'client_secret' => env('GOOGLE_CLIENT_SECRET'),
