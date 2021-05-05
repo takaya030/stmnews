5fdcfc0b2daced95ca1ae1229be050d7e317b38d
Minoru Takayama <takaya030@yahoo.co.jp>
2021-05-05 19:30:20 +0900

    add umarss controller

--- app/Http/Controllers/RssController.php
+++ app/Http/Controllers/RssController.php
@@ -6,6 +6,7 @@ use \Illuminate\Http\Request;
 use App\Models\Google\Datastore;
 use App\Models\Google\News\Item as NewsItem;
 use App\Models\Twitter\Tweet;
+use App\Models\Twitter\UmaTweet;
 use \Carbon\Carbon;
 use Google\Cloud\Datastore\DatastoreClient;
 
@@ -21,7 +22,7 @@ class RssController extends Controller
         //
     }
 
-    // test getting rss
+    // rss to twitter (stmnews)
 	public function getRss(Request $request)
 	{
 		$feed = new \SimplePie();
@@ -55,6 +56,75 @@ class RssController extends Controller
 				$url_list = $this->makeStoredUrlList( $datastore );
 
 				$tweet = new Tweet();
+
+				foreach( $data as $news )
+				{
+					if( !in_array( $news->getUrl(), $url_list, true ) )
+					{
+						$tweet->postNewsItem( $news );
+
+						$datastore->insertNewsItem( $news );
+
+						$last_timestamp = $news->getTimestamp();
+						$tweets_cnt++;
+						sleep(2);
+					}
+
+					if( $tweets_cnt >= $max_tweets )
+					{
+						break;
+					}
+				}
+
+			}
+
+			return response()->json([
+				'tweets_cnt' => $tweets_cnt,
+				'last_timestamp' => $last_timestamp,
+			]);
+		}
+		else
+		{
+			return response()->json([
+				'error' => $feed->error(),
+			]);
+		}
+	}
+
+    // rss to twitter (umamusu)
+	public function getUmarss(Request $request)
+	{
+		$feed = new \SimplePie();
+		$feed->set_feed_url( config('accounts.rss.uma_url') );
+		$feed->enable_cache(false); //キャッシュ機能はオフで使う
+		$success = $feed->init();
+		$feed->handle_content_type();
+
+		if ($success)
+		{
+			$data = [];
+			$oldest_timestamp = Carbon::now()->subHours(36)->timestamp;
+			foreach ($feed->get_items() as $item) {
+				$news = new NewsItem( $item );
+				if( $news->getTimestamp() > $oldest_timestamp )
+				{
+					array_unshift( $data, $news );
+				}
+			}
+
+			$max_tweets = 2;
+			$tweets_cnt = 0;
+			$last_timestamp = 0;
+			if( isset( $data[0] ) )
+			{
+				$dsc = new DatastoreClient([
+					'keyFilePath' => storage_path( config('accounts.google.key_file') )
+				]);
+				$datastore = new Datastore( $dsc, config('accounts.google.uma_datastore_kind') );
+
+				$url_list = $this->makeStoredUrlList( $datastore );
+
+				$tweet = new UmaTweet();
 
 				foreach( $data as $news )
 				{
