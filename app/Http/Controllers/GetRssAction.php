37db7b13b704d1358f881e34d7ca41bb7c8a7df8
Minoru Takayama <takaya030@yahoo.co.jp>
2021-12-30 11:35:43 +0900

    add GetRssAction

---
+++ app/Http/Controllers/GetRssAction.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Http\Controllers\Controller;
+
+use App\Domains\GetRssDomain as Domain;
+
+use App\Http\Responders\GetRssJsonResponder as Responder;
+
+class GetRssAction extends Controller
+{
+    protected $Domain;
+    protected $Responder;
+
+    public function __construct(Domain $Domain, Responder $Responder)
+    {
+        $this->Domain     = $Domain;
+        $this->Responder  = $Responder;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'limit' => 'required|integer|min:1|max:5',
+        ]);
+
+		$limit = (int)$request->input('limit');
+
+        return $this->Responder->response(
+            $this->Domain->get($limit)
+        );
+    }
+}
