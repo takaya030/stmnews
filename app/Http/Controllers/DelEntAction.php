3fd1ff6350d16006eb40d4e9ca3bdc839ab45c3a
Minoru Takayama <takaya030@yahoo.co.jp>
2021-12-30 17:48:34 +0900

    add DelEntAction

---
+++ app/Http/Controllers/DelEntAction.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Http\Controllers\Controller;
+
+use App\Domains\DelEntDomain as Domain;
+
+use App\Http\Responders\DelEntJsonResponder as Responder;
+
+class DelEntAction extends Controller
+{
+    protected $Domain;
+    protected $Responder;
+
+    public function __construct(Domain $Domain, Responder $Responder)
+    {
+        $this->Domain     = $Domain;
+        $this->Responder  = $Responder;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        return $this->Responder->response(
+            $this->Domain->get()
+        );
+    }
+}
