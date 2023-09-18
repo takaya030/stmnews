8e68d9d9f3f600bc0ee444f9607f4a5c203da494
Minoru Takayama <takaya030@yahoo.co.jp>
2023-09-18 02:25:03 +0000

    remove config of umamusu

--- config/accounts.php
+++ config/accounts.php
@@ -17,12 +17,6 @@ return [
 		'access_token'	=> env('TWITTER_ACCESS_TOKEN'),
 		'access_token_secret'	=> env('TWITTER_ACCESS_TOKEN_SECRET'),
 		'user_id'		=> env('TWITTER_USER_ID'),
-
-		'uma_client_id'		=> env('UMAMUSU_CLIENT_ID'),
-		'uma_client_secret'	=> env('UMAMUSU_CLIENT_SECRET'),
-		'uma_access_token'	=> env('UMAMUSU_ACCESS_TOKEN'),
-		'uma_access_token_secret'	=> env('UMAMUSU_ACCESS_TOKEN_SECRET'),
-		'uma_user_id'		=> env('UMAMUSU_USER_ID'),
 	],
 
 	/**
