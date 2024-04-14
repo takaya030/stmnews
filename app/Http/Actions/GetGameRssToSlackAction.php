647d9cea8e8e0a7830f1c30fbbebea48836c488d
takaya030-wsl2 <takaya030@yahoo.co.jp>
2024-04-14 19:02:29 +0900

    add game rss action

---
+++ app/Http/Actions/GetGameRssToSlackAction.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Actions;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Domains\GetRssToSlackDomain as Domain;
+use App\Http\Responders\GetRssToSlackJsonResponder as Responder;
+
+class GetGameRssToSlackAction extends Controller
+{
+    protected $Domain;
+
+    public function __construct(Domain $Domain)
+    {
+        $this->Domain     = $Domain;
+    }
+
+    /**
+     * @param Request $request
+     * @return GetRssJsonREsponder
+     */
+    public function __invoke(Request $request)
+    {
+        $this->validate($request, [
+            'limit' => 'required|integer|min:1|max:5',
+        ]);
+
+		$limit = (int)$request->input('limit');
+        $rss_url = config('accounts.game_rss.url');
+        $datastore_kind = config('accounts.google.game_datastore_kind');
+
+        return new Responder( $this->Domain->get($rss_url, $datastore_kind, $limit) );
+    }
+}
