080b0880e9c502a88979f2825aa6ff46dbbfe54e
Minoru Takayama <takaya030@yahoo.co.jp>
2024-10-05 18:26:47 +0900

    add timestamp

--- app/Models/Slack/Payload.php
+++ app/Models/Slack/Payload.php
@@ -9,16 +9,23 @@ class Payload
 {
 	protected	$title;
 	protected	$url;
+    protected   $timestamp;
 
 	/**
 	 * @param string $title
      * @param string $url
 	 */
-	public function __construct(string $title, string $url)
+	public function __construct(string $title, string $url, int $timestamp)
 	{
 		$this->title	= $title;   // news title
 		$this->url		= $url;     // news url
+        $this->timestamp    = $timestamp;
 	}
+
+    public function toArray(): array
+    {
+        return json_decode(json_encode($this), true);
+    }
 
 	public function getTitle(): string
 	{
@@ -28,5 +35,10 @@ class Payload
 	public function getUrl(): string
 	{
 		return $this->url;
+	}
+
+	public function getTimestamp(): int
+	{
+		return $this->timestamp;
 	}
 }
