ceab2ced3de0db1f97895e2f3b990be931b7deaa
Minoru Takayama <takaya030@yahoo.co.jp>
2024-10-26 17:22:18 +0900

    add class DatastoreRepositorySentNews

---
+++ app/Infrastructure/Repository/Datastore/DatastoreRepositorySentNews.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Infrastructure\Repository\Datastore;
+
+use App\Domain\Repository\IRepositorySentNews;
+
+class DatastoreRepositorySentNews extends DatastoreRepository implements IRepositorySentNews
+{
+    public function deleteAllThatBefore(int $oldest_timestamp)
+    {
+		$entities = $this->datastore->getBeforeAll( $oldest_timestamp );
+
+		$delents = [];
+		foreach( $entities as $entity )
+		{
+			$delents[] = $entity->key();
+		}
+
+		if( !empty( $delents ) )
+		{
+			$result = $this->datastore->deleteBatch( $delents );
+			app('log')->info('delete ent ids: ' . implode(",",$delents));
+		}
+
+		return [
+			'del_ents_cnt' => count($delents),
+		];
+    }
+}
