ce81ae77d39f7a1b61a48e4c80433ce5099fd462
Minoru Takayama <takaya030@yahoo.co.jp>
2024-11-16 14:31:40 +0900

    add function insertSNSPayload

--- app/Domain/Repository/IRepositorySentNews.php
+++ app/Domain/Repository/IRepositorySentNews.php
@@ -3,11 +3,13 @@
 namespace App\Domain\Repository;
 
 use App\Domain\Entity\News;
+use App\Domain\Entity\SNSPayload;
 
 interface IRepositorySentNews
 {
     public function setKind(string $kd): void;
     public function getAll(): array;
     public function insertNews(News $news): string;
+    public function insertSNSPayload(SNSPayload $payload): string;
     public function deleteAllThatBefore(int $oldest_timestamp): array;
 }
