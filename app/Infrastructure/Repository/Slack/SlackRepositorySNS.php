9760bd831a5caece70032da7ecaa843952ebcb17
Minoru Takayama <takaya030@yahoo.co.jp>
2024-11-16 11:55:32 +0900

    add function postSNSPayload

--- app/Infrastructure/Repository/Slack/SlackRepositorySNS.php
+++ app/Infrastructure/Repository/Slack/SlackRepositorySNS.php
@@ -3,25 +3,35 @@
 namespace App\Infrastructure\Repository\Slack;
 
 use App\Domain\Entity\News;
+use App\Domain\Entity\SNSPayload;
 use App\Domain\Repository\IRepositorySNS;
 
 class SlackRepositorySNS extends SlackRepository implements IRepositorySNS
 {
     /**
-	 * @param string $url
-	 * @return void
-	 */
+	   * @param string $url
+	   * @return void
+	   */
     public function setUrl(string $url): void
     {
         $this->url = $url;
     }
 
-    /**
-	 * @param App\Domain\Entity\News $news
-	 * @return string
-	 */
+   /**
+	  * @param App\Domain\Entity\News $news
+	  * @return string
+	  */
     public function postNews(News $news): string
     {
-		return $this->postText( $news->getTitle() . "\n" . $news->getUrl() );
+        return $this->postText( $news->getTitle() . "\n" . $news->getUrl() );
+    }
+
+   /**
+	  * @param App\Domain\Entity\SNSPayload $payload
+	  * @return string
+	  */
+    public function postSNSPayload(SNSPayload $payload): string
+    {
+        return $this->postText( $payload->getTitle() . "\n" . $payload->getUrl() );
     }
 }
